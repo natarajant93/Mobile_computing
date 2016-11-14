bfb3b809bcc4d9bf8502eee6bf3c258a59f5b743
natarajant93 <manoj.kumar0022@gmail.com>
2016-11-13 17:31:01 -0700

    Added download map pdf

--- app/src/main/java/com/example/natarajan/transitproject/MapsActivity.java
+++ app/src/main/java/com/example/natarajan/transitproject/MapsActivity.java
@@ -1,9 +1,14 @@
 package com.example.natarajan.transitproject;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.content.res.AssetManager;
 import android.database.sqlite.SQLiteDatabase;
+import android.net.Uri;
+import android.os.Environment;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -15,6 +20,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -59,6 +69,82 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 // currentContext.startActivity(activityChangeIntent);
 
                 MapsActivity.this.startActivity(activityChangeIntent);
+            }
+        }
+
+        );
+
+        Button download_button = (Button) findViewById(R.id.download_map);
+        download_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick (View v){
+                File fileBrochure = new File(Environment.getExternalStorageDirectory() + "/" + "abc.pdf");
+                if (!fileBrochure.exists())
+                {
+                    CopyAssetsbrochure();
+                }
+
+                /** PDF reader code */
+                File file = new File(Environment.getExternalStorageDirectory() + "/" + "abc.pdf");
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setDataAndType(Uri.fromFile(file),"application/pdf");
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                try
+                {
+                    getApplicationContext().startActivity(intent);
+                }
+                catch (ActivityNotFoundException e)
+                {
+                    Toast.makeText(MapsActivity.this, "NO Pdf Viewer", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            //method to write the PDFs file to sd card
+            private void CopyAssetsbrochure() {
+                AssetManager assetManager = getAssets();
+                String[] files = null;
+                try
+                {
+                    files = assetManager.list("");
+                }
+                catch (IOException e)
+                {
+                    Log.e("tag", e.getMessage());
+                }
+                for(int i=0; i<files.length; i++)
+                {
+                    String fStr = files[i];
+                    if(fStr.equalsIgnoreCase("abc.pdf"))
+                    {
+                        InputStream in = null;
+                        OutputStream out = null;
+                        try
+                        {
+                            in = assetManager.open(files[i]);
+                            out = new FileOutputStream(Environment.getExternalStorageDirectory() + "/" + files[i]);
+                            copyFile(in, out);
+                            in.close();
+                            in = null;
+                            out.flush();
+                            out.close();
+                            out = null;
+                            break;
+                        }
+                        catch(Exception e)
+                        {
+                            Log.e("tag", e.getMessage());
+                        }
+                    }
+                }
+            }
+
+            private void copyFile(InputStream in, OutputStream out) throws IOException {
+                byte[] buffer = new byte[1024];
+                int read;
+                while((read = in.read(buffer)) != -1){
+                    out.write(buffer, 0, read);
+                }
             }
         }
 
